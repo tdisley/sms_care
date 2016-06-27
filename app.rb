aec2cae8b0e071f273187c11ed9b64eb3dfd55e0
neodevelop <neodevelop@gmail.com>
2016-06-27 17:53:12 -0500

    #2 saving and responding

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './config/environments' #database configuration
-require './models/model'
+require './models/message'
 require 'json'
 
 get '/' do
@@ -11,32 +11,40 @@ end
 
 post '/' do
   message = Message.new(params)
-  #secret = params['secret']
-  #from = params['from']
-  #message = params['message']
-  #sent_timestamp = params['sent_timestamp']
-  #sent_to = params['sent_to']
-  #message_id = params['message_id']
-  #device_id = params['device_id']
-  puts "#{secret}|#{from}|#{message}|#{sent_timestamp}|#{sent_to}|#{message_id}|#{device_id}"
+  p message
   content_type :json
-  {
-    "payload" =>
+  if model.save
     {
-      "success" => true,
-      "error" => nil,
-      "task": "send",
-      "messages": [
-        {
-          "to": "#{from}",
-          "message": "Replying from intelligence",
-          "uuid": "#{message_id}"
-        }
-      ]
-    }
-  }.to_json
+      "payload" =>
+      {
+        "success" => true,
+        "error" => nil,
+        "task": "send",
+        "messages": [
+          {
+            "to": "#{message.from}",
+            "message": "Replying from intelligence",
+            "uuid": "#{message.message_id}"
+          }
+        ]
+      }
+    }.to_json
+  else
+    {
+      "payload" =>
+      {
+        "success" => false,
+        "error" => "Cannot save the message",
+        "task": "send",
+        "messages": [
+          {
+            "to": "#{message.from}",
+            "message": "Try  again",
+            "uuid": "#{message.message_id}"
+          }
+        ]
+      }
+    }.to_json
+  end
 end
 
-post '/callback' do
-  "Hello World!"
-end
