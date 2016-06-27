139c5914da9a962aca796d4b68ad709d913a6945
neodevelop <neodevelop@gmail.com>
2016-06-27 17:43:17 -0500

    #2 adding the migration for messages

---
+++ db/migrate/20160627223209_create_message_model.rb
@@ -0,0 +1,14 @@
+class CreateMessageModel < ActiveRecord::Migration
+  # TODO: Maybe refactor the types
+  def change
+    create_table :messages do |t|
+      t.column :secret, :string
+      t.column :from, :string
+      t.column :message, :string
+      t.column :sent_timestamp, :string
+      t.column :sent_to, :string
+      t.column :message_id, :string
+      t.column :device_id, :string
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627202547) do
+ActiveRecord::Schema.define(version: 20160627223209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "secret"
+    t.string "from"
+    t.string "message"
+    t.string "sent_timestamp"
+    t.string "sent_to"
+    t.string "message_id"
+    t.string "device_id"
+  end
 
   create_table "models", force: :cascade do |t|
     t.string "name"
