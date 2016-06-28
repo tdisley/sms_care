a01bc07ddbee3d6f0665fb7c8080e2acfd692df4
neodevelop <neodevelop@gmail.com>
2016-06-28 11:39:23 -0500

    #4 adding more tests

--- spec/responses_spec.rb
+++ spec/responses_spec.rb
@@ -1,10 +1,14 @@
 require './lib/auto_response'
 
 RSpec.describe AutoResponse do
-  it "should response the right message depending on the code" do
-    message = "Some 111 some"
-    response = AutoResponse.instance.respond_to(message)
-    expect(response).to eq("Thank you for contacting SMS Care. Our team will contact you soon for a free care referral.")
+  [
+    ["some 111 some","Thank you for contacting SMS Care. Our team will contact you soon for a free care referral."],
+    ["some some","Please write us and include some code: 111 for contact, 222 for ,eet you better, 333 for mor info"],
+  ].each do | sms, response |
+    it "SMS is #{sms} and response is #{response}" do
+      current_response = AutoResponse.instance.respond_to(sms)
+      expect(current_response).to eq(response)
+  end
   end
 end
 
