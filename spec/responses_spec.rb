3ac22f374c10bdbbfbe06f1e471b86d78c690ab1
neodevelop <neodevelop@gmail.com>
2016-06-28 13:24:20 -0500

    #4 adding another test

--- spec/responses_spec.rb
+++ spec/responses_spec.rb
@@ -3,7 +3,8 @@ require './lib/auto_response'
 RSpec.describe AutoResponse do
   [
     ["some 111 some","Thank you for contacting SMS Care. Our team will contact you soon for a free care referral."],
-    ["some some","Please write us and include some code: 111 for contact, 222 for ,eet you better, 333 for mor info"],
+    ["some some","Please write us and include some code: 111 for contact, 222 for meet you better, 333 for mor info"],
+    ["another response with 222","Thank you for contacting SMS Care. What is the name and age of your child? end with 221 code"]
   ].each do | sms, response |
     it "SMS is #{sms} and response is #{response}" do
       current_response = AutoResponse.instance.respond_to(sms)
